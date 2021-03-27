add983fd2c48cf6d44da0607b88a73f105ca50e7
Subha Azrin <74439604+subhaazrin@users.noreply.github.com>
2021-03-26 20:13:04 -0400

    Create CCC-j1-2015.py

---
+++ CCC-j1-2015.py
@@ -0,0 +1,13 @@
+month = int(input())
+day =int(input())
+
+if month ==2 and day == 18:
+  print("Special")
+elif month < 2:
+  print("Before")
+elif month> 2:
+  print("After")
+elif month == 2 and day > 18:
+  print("After")
+else:
+  print("Before")
